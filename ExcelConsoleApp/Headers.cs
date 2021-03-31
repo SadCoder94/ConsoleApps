267c2b9709a92e264175d8337f9722daca3e9bde
SadCoder94 <anthony.dhara94@gmail.com>
2021-03-31 21:31:14 +0530

    Add project files.

---
+++ ExcelConsoleApp/Headers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcelConsoleApp
+{
+    class Headers
+    {
+        public string col1 { get; set; }
+        public string col2 { get; set; }
+        public string col3 { get; set; }
+    }
+}
---
+++ ExcelConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using Newtonsoft.Json;
+using OfficeOpenXml;
+
+namespace ExcelConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<Headers> data =  ReadFromExcel<List<Headers>>(filePath);
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.col1+" "+item.col2+" "+item.col3);
+            }
+        }
+        public static string filePath = @"D:\TrialExcel.xlsx";
+        //https://www.thecodebuzz.com/read-write-excel-in-dotnet-core-epplus/
+
+        private static T ReadFromExcel<T>(string path, bool hasHeader = true)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var excelPack = new ExcelPackage())
+            {
+                //Load excel stream
+                using (var stream = File.OpenRead(path))
+                {
+                    excelPack.Load(stream);
+                }
+
+                //Lets Deal with first worksheet.(You may iterate here if dealing with multiple sheets)
+                var ws = excelPack.Workbook.Worksheets[0];
+
+                //Get all details as DataTable -because Datatable make life easy :)
+                DataTable excelasTable = new DataTable();
+                foreach (var firstRowCell in ws.Cells[1, 1, 1, ws.Dimension.End.Column])
+                {
+                    //Get colummn details
+                    if (!string.IsNullOrEmpty(firstRowCell.Text))
+                    {
+                        string firstColumn = string.Format("Column {0}", firstRowCell.Start.Column);
+                        excelasTable.Columns.Add(hasHeader ? firstRowCell.Text : firstColumn);
+                    }
+                }
+                var startRow = hasHeader ? 2 : 1;
+                //Get row details
+                for (int rowNum = startRow; rowNum <= ws.Dimension.End.Row; rowNum++)
+                {
+                    var wsRow = ws.Cells[rowNum, 1, rowNum, excelasTable.Columns.Count];
+                    DataRow row = excelasTable.Rows.Add();
+                    foreach (var cell in wsRow)
+                    {
+                        row[cell.Start.Column - 1] = cell.Text;
+                    }
+                }
+                //Get everything as generics and let end user decides on casting to required type
+                var generatedType = JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(excelasTable));
+                return (T)Convert.ChangeType(generatedType, typeof(T));
+            }
+        }
+    }
+}
